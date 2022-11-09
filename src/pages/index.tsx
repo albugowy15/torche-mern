e58347ee9a088eee04ae11451452f49dab5be1a2
Natabagas <meganatabagas@gmail.com>
2022-11-09 21:31:23 +0700

    Menambahkan component Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,16 @@ import { NextPage } from 'next';
 import React from 'react';
 import HomePage from './home';
 import styles from "../styles/Home.module.css";
+import Header from "../components/header/Header";
 
 
 const RootPage: NextPage = () => {
-  return <HomePage />;
+  return (
+  <div>
+    <Header />
+    <HomePage />
+  </div>
+  );
 }
 
 export default RootPage

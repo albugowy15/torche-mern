f87e5f7a01a5b1a585be21f205c80c2fd18445e0
adelinejune <adeline0721@gmail.com>
2022-11-18 09:18:46 +0800

    [+] Create Course Card Component

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import React, { useState } from 'react';
 import Image from 'next/image';
 import COURSESDATA from '../../data/courses'
+import CoursesCard from '@/components/CoursesCard'
 
 const  CoursesPage: NextPage = () =>  {
   const [coursesData, setCoursesData] = useState(COURSESDATA);
@@ -28,29 +29,13 @@ const  CoursesPage: NextPage = () =>  {
         <h3 className='mb-0'>Courses</h3>
         <div>Courses / Our Courses</div>
       </div>
-      {
-        coursesData.map((course) => (
-          <div key={course.id} className="w-[410px] h-[530px] border rounded flex flex-col cursor-pointer hover:scale-[1.02] hover:shadow">
-            <div className='w-full h-[200px] bg-cover bg-center rounded-tl rounded-tr' style={{backgroundImage: `url(${course.image.src})`}}></div>
-            {/* <img src={course.image.src} alt="decoration"/> */}
-            <div className='flex flex-col justify-between h-[330px]'>
-              <div>
-                <div className='flex justify-between items-center p-2.5'>
-                  <p className='bg-[#f5faff] p-2 rounded font-bold'>{course.category}</p>
-                  <p>{course.rating} / 10</p>
-                </div>
-                <div className='p-2 pt-0'>
-                  <h4 className='font-bold'>{course.card_title}</h4>
-                  <p className='text-[#777777]'>{course.card_desc}</p>
-                </div>
-              </div>
-              <div className='px-2.5 py-3  border-t'>
-                {course.tutors.length > 1 ? `${course.tutors.length} tutors` : `${course.tutors.length} tutor`}
-              </div>
-            </div>
-          </div>
-        ))
-      }
+      <div className='p-8 lg:p-16 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {
+          coursesData.map((course) => (
+            <CoursesCard key={course.id} course={course}/>
+          ))
+        }
+      </div>
     </div>
   ) 
 }

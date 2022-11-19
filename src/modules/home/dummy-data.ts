4c70406ef0347204d4f2788626d4ae3f7c58e7b6
albugowy15 <kholidbughowi@gmail.com>
2022-11-19 15:44:36 +0700

    add dummy data

---
+++ src/modules/home/dummy-data.ts
@@ -0,0 +1,58 @@
+import { Easel2, People, PersonVideo3, Mortarboard, Pen, Book, Calendar4Week, CameraReels, ChatSquareText } from "react-bootstrap-icons";
+
+export const ListServices = [
+	{
+		Icon: Easel2,
+		title: "Lecturing Class",
+		description: "Kelas tutor terdiri dari penjelasan materi dari tutor kami disertai contoh pemecahan masalahnya khususnya di bidang teknik kimia.",
+		color: "#FF6442",
+	},
+	{
+		Icon: People,
+		title: "Consultation Class",
+		description: "Siswa dapat bertanya tentang materi, pekerjaan rumah, tugas, dan proyek yang berkaitan dengan topik yang dipilih.",
+		color: "#FFAE5F",
+	},
+	{
+		Icon: PersonVideo3,
+		title: "Exam Preparation Class",
+		description: "Kelas persiapan ujian difokuskan bagi siswa yang ingin mengikuti ujian dengan materi yang tersedia.",
+		color: "#51C1CB",
+	},
+	{
+		Icon: Mortarboard,
+		title: "Kelas Khusus (Advanced)",
+		description: "Torche menyediakan kelas custom yang materi pembelajarannya bisa mengikuti kebutuhan siswa.",
+		color: "#41cf2e",
+	},
+	{
+		Icon: Pen,
+		title: "Kelas Penulisan Karya Ilmiah",
+		description: "Kelas yang disediakan bagi siswa yang membutuhkan bimbingan untuk skripsi, thesis, atau penulisan karya ilmiah lainnya.",
+		color: "#5885E9",
+	},
+	{
+		Icon: Book,
+		title: "Kelas Sekolah Menengah Pertama dan Atas",
+		description: "Kelas yang disediakan bagi siswa sekolah menengah (SMP, SMA, dan SMK), termasuk persiapan SBMPTN dan A-Level Test.",
+		color: "#0B122A",
+	},
+];
+
+export const ListBenefits = [
+	{
+		icon: Calendar4Week,
+		title: "Flexible Time",
+		description: "Waktu bisa diatur berdasarkan kesepakatan antara siswa dan tutor.",
+	},
+	{
+		icon: CameraReels,
+		title: "Free Class Recordings",
+		description: "Gratis tanpa dipungut biaya apapun.",
+	},
+	{
+		icon: ChatSquareText,
+		title: "After-class Consultation",
+		description: "Berlaku selama 7 hari setelah kelas dengan bergabung ke server Discord kami.",
+	},
+];

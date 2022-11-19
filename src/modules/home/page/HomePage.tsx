d4f3adf53b8deda41e40806e3f6db4c0ff68561f
albugowy15 <kholidbughowi@gmail.com>
2022-11-19 15:44:51 +0700

    add services and benefits

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../components/HeroSection";
 import Profile from "../components/Profile";
 import Services from "../components/Services";
+import Tutoring from "../components/Tutoring";
 
 export default function HomePage() {
 	return (
@@ -8,6 +9,7 @@ export default function HomePage() {
 			<HeroSection />
 			<Profile />
 			<Services />
+			<Tutoring />
 		</>
 	);
 }
